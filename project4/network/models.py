15e8dbfe2fdce3bc04f0505df8624fbc6ee5dbef
unknown <corbettjustis@gmail.com>
2020-08-05 17:55:33 -0600

    Post and Following db models

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    likes = models.ManyToManyField(User)
+
+
+class Following(models.Model):
+    target = target = models.ForeignKey(
+        User,
+        related_name='followers',
+        on_delete=models.CASCADE
+    )
+    follower = models.ForeignKey(
+        User, 
+        related_name='targets',
+        on_delete=models.CASCADE
+    )
