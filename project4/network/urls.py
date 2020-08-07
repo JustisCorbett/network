dc3187db72de95099fdc3cee8765a0d46bf7d865
unknown <corbettjustis@gmail.com>
2020-08-07 16:41:17 -0600

    added urls for follow, like, and edit_post

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("create_post", views.create_post, name="create_post")
+    path("create_post", views.create_post, name="create_post"),
+    path("follow", views.follow, name="follow"),
+    path("like", views.like, name="like"),
+    path("edit_post", views.edit_post, name="edit_post")
 ]
